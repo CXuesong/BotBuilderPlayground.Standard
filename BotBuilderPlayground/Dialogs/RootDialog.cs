bdeb13e42a68635dbdfc8901c61d88fdf17be188
forest93 <forest93@163.com>
2017-12-24 22:08:25 +0800

    Checkpoint.

--- BotBuilderPlayground/Dialogs/RootDialog.cs
+++ BotBuilderPlayground/Dialogs/RootDialog.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Connector;
 
 namespace BotBuilderPlayground.Dialogs
 {
@@ -18,8 +20,32 @@ namespace BotBuilderPlayground.Dialogs
             {
                 var message = await awaitable;
                 await ctx.PostAsync("You said " + message.Text);
-                ctx.Done<object>(null);
+                await NoneIntent(ctx, null);
             });
         }
+
+        public async Task NoneIntent(IDialogContext context, LuisResult result)
+        {
+            PromptDialog.Confirm(context, AfterAskInvestigate, "Would you like us to investigate and follow-up?");
+        }
+
+        private async Task AfterAskInvestigate(IDialogContext context, IAwaitable<bool> result)
+        {
+            var investigate = await result;
+
+            if (investigate)
+                await context.PostAsync("OK, we're on the case.");
+            else
+                await context.PostAsync("No problem, we'll leave it.");
+
+            context.Wait(MessageReceived);
+        }
+
+        private async Task MessageReceived(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            await context.PostAsync("TODO MessageReceived");
+            context.Done<object>(null);
+        }
+
     }
 }
